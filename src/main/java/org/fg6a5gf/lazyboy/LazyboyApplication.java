ff4d61c05dc5f46d3e7f92ad797c9e0c52d8caed
fg6a5gf <zhengxiuhang@gmail.com>
2022-12-26 15:19:15 +0800

    first commit

---
+++ src/main/java/org/fg6a5gf/lazyboy/LazyboyApplication.java
@@ -0,0 +1,13 @@
+package org.fg6a5gf.lazyboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyboyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyboyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fg6a5gf/lazyboy/LazyboyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fg6a5gf.lazyboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
